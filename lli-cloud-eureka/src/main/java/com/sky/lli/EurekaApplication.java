f308554c9d2c0f9e5176452088dd6fc170b07858
lihao <15215401693@163.com>
2020-08-17 17:02:20 +0800

    Ftp模块初版,待完善

--- lli-cloud-eureka/src/main/java/com/sky/lli/EurekaApplication.java
+++ lli-cloud-eureka/src/main/java/com/sky/lli/EurekaApplication.java
@@ -13,9 +13,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
