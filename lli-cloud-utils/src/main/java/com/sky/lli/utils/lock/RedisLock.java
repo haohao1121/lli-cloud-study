6ffded70061e622b1fecda3e6f7790d177a8e1e3
lihao <15215401693@163.com>
2020-12-02 15:05:14 +0800

    zookeeper分布式锁,redisson分布式锁初始化

--- lli-cloud-utils/src/main/java/com/sky/lli/utils/lock/RedisLock.java
+++ lli-cloud-utils/src/main/java/com/sky/lli/utils/lock/RedisLock.java
@@ -5,7 +5,6 @@ import com.sky.lli.utils.exception.UtilsException;
 import com.sky.lli.utils.thread.NativeAsyncTaskExecutePool;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
@@ -25,7 +24,6 @@ import java.util.function.Supplier;
  */
 @Slf4j
 @Component
-@ConditionalOnProperty(name = "lli.redis.lock.enable", havingValue = "true")
 public class RedisLock {
 
     private final StringRedisTemplate redisTemplate;
