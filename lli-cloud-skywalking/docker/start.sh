845cd076cb94369bec1293b7fb174a97b5e31f9a
lihao <15215401693@163.com>
2020-12-25 18:34:08 +0800

    优化

---
+++ lli-cloud-skywalking/docker/start.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# 验证参数
+if  [ ! -n "$KAFKA_IP" ] ;then
+    echo "KAFKA_IP is not set!"
+    exit
+fi
+if  [ ! -n "$APP_NAME" ] ;then
+    echo "APP_NAME is not set!"
+    exit
+fi
+
+# filebeat kafka参数
+sed -i "s/kafkahost/${KAFKA_IP}/g" /data/filebeat/filebeat.yml
+sed -i "s/unkown-topic/${APP_NAME}/g" /data/filebeat/filebeat.yml
+# 处理权限问题
+chown root /data/filebeat/filebeat.yml
+nohup /data/filebeat/filebeat -e -c /data/filebeat/filebeat.yml >info.log 2>&1 &
+
+# 方式一 使用 APP_NAME 和 SW_COLLECTOR_IP 参数
+#java -javaagent:/data/agent/skywalking-agent.jar -Dskywalking.agent.service_name=${APP_NAME} -Dskywalking.collector.backend_service=${SW_COLLECTOR_IP}:11800 -jar /data/app.jar
+# 方式二 使用 SW_AGENT_NAME 和  SW_AGENT_COLLECTOR_BACKEND_SERVICES 参数
+java -javaagent:/data/agent/skywalking-agent.jar -jar /data/app.jar
+
