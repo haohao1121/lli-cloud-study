245111084a781b29ef8a37b76bc186d920293116
lihao <15215401693@163.com>
2021-01-19 17:28:49 +0800

    优化

--- lli-cloud-fastdfs/src/main/java/com/sky/lli/controller/UploadController.java
+++ lli-cloud-fastdfs/src/main/java/com/sky/lli/controller/UploadController.java
@@ -21,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -190,7 +191,7 @@ public class UploadController {
         if (value < fileUnitSize) {
             return value + "B";
         } else {
-            value = BigDecimal.valueOf(value / fileUnitSize).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();
+            value = BigDecimal.valueOf(value / fileUnitSize).setScale(2, RoundingMode.DOWN).doubleValue();
         }
         // 如果原字节数除于1024之后，少于1024，则可以直接以KB作为单位
         // 因为还没有到达要使用另一个单位的时候
@@ -198,13 +199,13 @@ public class UploadController {
         if (value < fileUnitSize) {
             return value + "KB";
         } else {
-            value = BigDecimal.valueOf(value / fileUnitSize).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();
+            value = BigDecimal.valueOf(value / fileUnitSize).setScale(2, RoundingMode.DOWN).doubleValue();
         }
         if (value < fileUnitSize) {
             return value + "MB";
         } else {
             // 否则如果要以GB为单位的，先除于1024再作同样的处理
-            value = BigDecimal.valueOf(value / fileUnitSize).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();
+            value = BigDecimal.valueOf(value / fileUnitSize).setScale(2, RoundingMode.DOWN).doubleValue();
             return value + "GB";
         }
     }
