24454aaeeae1b672f3f3760cd5f9dbb0fe30ecaa
klaus <15215401693@163.com>
2021-04-20 00:59:12 +0800

    cas-rest-auth

---
+++ lli-cloud-sso/lli-cloud-sso-cas-auth/src/main/java/com/sky/lli/CasRestAuthApplication.java
@@ -0,0 +1,17 @@
+package com.sky.lli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主函数入口
+ *
+ * @author klaus
+ */
+@SpringBootApplication
+public class CasRestAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasRestAuthApplication.class, args);
+    }
+}
